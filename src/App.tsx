47e6921a565cc280047e95f2e15a36f85af1cbd2
Kofi Boakye <66253143+boakyezitek@users.noreply.github.com>
2022-07-03 21:49:38 +0000

    configure babel and webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header>
+       <AppLogo logo={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Link href="https://reactjs.org">Learn React</Link>
+      </Header>
     </div>
   );
 }
 
+interface Props {
+  children: React.ReactNode;
+}
+
+const Header: FC<Props> = ({children}) => {
+  return <header className="App-header">
+    {children}
+  </header>;
+}
+
+interface LinkProps {
+  children: React.ReactNode;
+  href: string;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  rel?: string;
+}
+
+const Link: FC<LinkProps> = ({children,  ...resProps}) => {
+  return <React.Fragment>
+    <a className="App-link" { ...resProps }></a>
+    {children}
+  </React.Fragment> 
+}
+
+interface AppLogoProps {
+  logo?: string
+}
+
+const AppLogo: FC<AppLogoProps> = ({logo}) => {
+  return <>
+  {Boolean(logo) && <img src={logo} className="App-logo" alt="logo" />}
+  {Boolean(logo )|| "No image found"}
+  </> 
+}
+Link.defaultProps={
+  target:"_top",
+  rel:"noopener noreferrer"
+}
 export default App;
